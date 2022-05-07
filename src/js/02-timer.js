b5480861dd47518341a2e4422426213c4e149ee7
Kostyantyn Krivka <koskvk@gmail.com>
2022-05-07 22:07:55 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,12 +6,13 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
         onClose(selectedDates) {
-            dateSelectedByTheUser(selectedDates[0]);
-    },
+            // dateSelectedByTheUser(selectedDates[0]);
+            console.log(selectedDates[0]);
+        },
 };
 const inputEl = document.querySelector("#datetime-picker");
 const fp = flatpickr(inputEl, { options });
 
-function dateSelectedByTheUser(date) {
-    console.log(date);
-};
+// function dateSelectedByTheUser(date) {
+//     console.log(date);
+// };
