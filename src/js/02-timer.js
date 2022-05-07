152dabe529484380cdd3a5ce60ccbd1c614d8df5
Kostyantyn Krivka <koskvk@gmail.com>
2022-05-07 20:55:35 +0300

    task2

---
+++ src/js/02-timer.js
@@ -0,0 +1,17 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+      dateSelectedByTheUser(selectedDates[0]);
+  },
+};
+const inputEl = document.querySelector("#datetime-picker");
+const fp = flatpickr(inputEl, { options });
+
+function dateSelectedByTheUser(date) {
+    console.log(date);
+};
